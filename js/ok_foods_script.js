26128d5d88cf8fd0fcb5d54e40b06760582dba81
Joey05010 <andzijoey05@gmail.com>
2025-07-04 22:48:38 +0200

    Add files via upload

---
+++ js/ok_foods_script.js
@@ -0,0 +1,13 @@
+const images = document.querySelectorAll('.frames img');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+images.forEach(img => observer.observe(img));
